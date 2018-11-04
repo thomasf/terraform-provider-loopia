09973f0ef516a1e2bdc2bdea5a6d71ea01ca27b0
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-11-04 23:56:23 +0100

    go fmt

--- loopia/provider.go
+++ loopia/provider.go
@@ -1,26 +1,45 @@
 package loopia
 
 import (
-    "github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
 )
 
+// Provider -
 func Provider() terraform.ResourceProvider {
-    return &schema.Provider{
-        Schema: map[string]*schema.Schema{
-            "username": &schema.Schema{
-              Type:        schema.TypeString,
-              Required:    true,
-              DefaultFunc: schema.EnvDefaultFunc("LOOPIA_USERNAME", nil),
-            },
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("LOOPIA_USERNAME", nil),
+			},
 
-            "password": &schema.Schema{
-              Type:        schema.TypeString,
-              Required:    true,
-              DefaultFunc: schema.EnvDefaultFunc("LOOPIA_PASSWORD", nil),
-            },
-        },
-        ResourcesMap: map[string]*schema.Resource{
-            "loopia_record": resourceLoopiaRecord(),
-        },
-    }
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("LOOPIA_PASSWORD", nil),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"loopia_record":    resourceLoopiaRecord(),
+			"loopia_subdomain": resourceLoopiaSubdomain(),
+		},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		Username: d.Get("username").(string),
+		Password: d.Get("password").(string),
+	}
+
+	client, err := config.Client()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
--- loopia/resource_loopia_record.go
+++ loopia/resource_loopia_record.go
@@ -1,96 +1,99 @@
 package loopia
 
 import (
-    "github.com/hashicorp/terraform/helper/schema"
-    "github.com/jonlil/loopia-go"
-    "fmt"
-    "strconv"
-    "strings"
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/jonlil/loopia-go"
+	"strconv"
+	"strings"
 )
 
 func resourceLoopiaRecord() *schema.Resource {
-    return &schema.Resource{
-        Create: resourceLoopiaRecordCreate,
-        Read:   resourceLoopiaRecordRead,
-        Update: resourceLoopiaRecordUpdate,
-        Delete: resourceLoopiaRecordDelete,
-        Importer: &schema.ResourceImporter{
-            State: resourceLoopiaeRecordImport,
-        },
+	return &schema.Resource{
+		Create: resourceLoopiaRecordCreate,
+		Read:   resourceLoopiaRecordRead,
+		Update: resourceLoopiaRecordUpdate,
+		Delete: resourceLoopiaRecordDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceLoopiaeRecordImport,
+		},
 
-        Schema: map[string]*schema.Schema{
-            "domain": &schema.Schema{
-                Type:     schema.TypeString,
-                Required: true,
-            },
-            "name": &schema.Schema{
-                Type:     schema.TypeString,
-                Required: true,
-            },
-            "value": &schema.Schema{
-                Type:     schema.TypeString,
-                Required: true,
-            },
-            "type": &schema.Schema{
-                Type:     schema.TypeString,
-                Required: true,
-            },
-            "ttl": &schema.Schema{
-                Type:     schema.TypeInt,
-                Optional: true,
-                Default:  3600,
-            },
-            "priority": &schema.Schema{
-                Type:     schema.TypeInt,
-                Optional: true,
-                Default:  0,
-            },
-        },
-    }
+		Schema: map[string]*schema.Schema{
+			"domain": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"value": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"ttl": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  3600,
+			},
+			"priority": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  0,
+			},
+		},
+	}
 }
 
 func resourceLoopiaRecordCreate(d *schema.ResourceData, m interface{}) error {
-    return resourceLoopiaRecordRead(d, m)
+	return resourceLoopiaRecordRead(d, m)
 }
 
-func resourceLoopiaRecordRead(d *schema.ResourceData, m interface{}) error {
-    record, err := loopia.GetZoneRecord()
-    if err != nil {
-      return nil, fmt.Errorf("Unable to find record with ID %q: %q", d.Id())
-    }
-    return nil
+func resourceLoopiaRecordRead(d *schema.ResourceData, meta interface{}) error {
+	// client := meta.(*loopia.API)
+	return nil
+	// record, err := client.GetZoneRecord()
+	// if err != nil {
+	//   return nil, fmt.Errorf("Unable to find record with ID %q: %q", d.Id())
+	// }
+	// return nil
 }
 
 func resourceLoopiaRecordUpdate(d *schema.ResourceData, m interface{}) error {
-    return resourceLoopiaRecordRead(d, m)
+	return resourceLoopiaRecordRead(d, m)
 }
 
 func resourceLoopiaRecordDelete(d *schema.ResourceData, m interface{}) error {
-    return nil
+	return nil
 }
 
 func resourceLoopiaeRecordImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
-    idAttr := strings.SplitN(d.Id(), "/", 3)
-    var domain string
-    var subdomain string
+	client := meta.(*loopia.API)
+	idAttr := strings.SplitN(d.Id(), "/", 3)
+	var domain string
+	var subdomain string
 
-    id, _ := strconv.ParseInt(idAttr[2], 10, 0)
-    domain = idAttr[1]
-    subdomain = idAttr[0]
+	id, _ := strconv.ParseInt(idAttr[2], 10, 0)
+	domain = idAttr[1]
+	subdomain = idAttr[0]
 
-    record, err := loopia.GetZoneRecord(domain, subdomain, id)
+	record, err := client.GetZoneRecord(domain, subdomain, id)
 
-    if err != nil {
-        return nil, fmt.Errorf("Unable to find record with ID %q: %q", id, err)
-    }
+	if err != nil {
+		return nil, fmt.Errorf("Unable to find record with ID %q: %q", id, err)
+	}
 
-    d.Set("name", subdomain)
-    d.Set("domain", domain)
-    d.Set("priority", record.Priority)
-    d.Set("value", record.Value)
-    d.Set("type", record.Type)
-    d.Set("ttl", record.Ttl)
-    d.SetId(idAttr[2])
+	d.Set("name", subdomain)
+	d.Set("domain", domain)
+	d.Set("priority", record.Priority)
+	d.Set("value", record.Value)
+	d.Set("type", record.Type)
+	d.Set("ttl", record.TTL)
+	d.SetId(idAttr[2])
 
-    return []*schema.ResourceData{d}, nil
+	return []*schema.ResourceData{d}, nil
 }
